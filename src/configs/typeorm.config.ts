d231b0f8ff29f0d681fd1cb5aea844ac2b52c75f
Fhenriqueoliveira <fhenrique.sistemas@gmail.com>
2021-10-26 21:46:45 -0300

    atualizacao
    Signed-off-by: Fhenriqueoliveira <fhenrique.sistemas@gmail.com>

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -5,7 +5,7 @@ export const typeormconfig: TypeOrmModuleOptions = {
   host: 'localhost',
   port: 5432,
   username: 'postgres',
-  password: '55416354',
+  password: 'senha',
   database: 'steam',
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: true,
